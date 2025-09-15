a80be8ff71de59232029a211c3caaf159869656b
D-pad25 <daniel.padbury@gmail.com>
2025-09-15 22:34:17 +1000

    trying A100

--- train_script.sh
+++ train_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 #PBS -N DIFFUSION_TRAIN
-#PBS -l walltime=32:00:00
-#PBS -l select=1:ncpus=24:ngpus=1:gpu_id=H100:mem=160gb
+#PBS -l walltime=33:00:00
+#PBS -l select=1:ncpus=24:ngpus=1:gpu_id=A100:mem=200gb
 
 set -euo pipefail
 
